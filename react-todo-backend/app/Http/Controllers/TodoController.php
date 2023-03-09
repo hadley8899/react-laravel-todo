3cc2ccb9747ddcb456b4b389aa70c6c9b7f48f87
ryan <hadley8899@gmail.com>
2023-03-09 21:55:36 +0000

    Basics done

--- react-todo-backend/app/Http/Controllers/TodoController.php
+++ react-todo-backend/app/Http/Controllers/TodoController.php
@@ -15,7 +15,7 @@ class TodoController extends Controller
      */
     public function index()
     {
-        return TodoCollection::collection(Todo::all());
+        return new TodoCollection(Todo::all());
     }
 
     /**
